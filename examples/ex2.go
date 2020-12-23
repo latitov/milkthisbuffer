0348268c0c43abf79a900107016972cd71eeb8ea
Leonid Titov, CS <5195118@gmail.com>
2020-12-23 10:29:46 +0300

    Add files via upload

---
+++ examples/ex2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/latitov/milkthisbuffer"
+	"time"
+)
+
+// Ctrl-D to end the input (Ctrl-Z on Windows)
+
+func main() {
+	pipe1 := milkthisbuffer.New(500)
+	pipe2 := milkthisbuffer.New(500)
+	
+	go pipe2.StdoutAsync()
+	
+	co1 := &milkthisbuffer.CommandObject{
+		Stdout: pipe1,
+		Stderr: pipe1,
+	}
+	co2 := &milkthisbuffer.CommandObject{
+		Stdin: pipe1,
+		Stdout: pipe2,
+		Stderr: pipe2,
+	}
+	
+	go func() {
+		co2.Execf("tr", "a-z", "A-Z")
+	}()
+	
+	co1.Execf("echo", "hey-hi-hello, 1-2-3, repeat")
+	
+	pipe1.Close()	// signal the EOF
+	
+	time.Sleep(1 * time.Second)
+}
